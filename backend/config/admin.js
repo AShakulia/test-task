c118f526b741da8e322fca68ed07e643f032090f
Andrei Shakulia <ashakulia98@gmail.com>
2024-09-07 17:53:26 +0300

    fix

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'eEvUYssHNDf8WR51oaMM1A=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'hx5BSGJxYmx5w68AaI+Tpg=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'V0SOwSAjQiHicl7adFdt8A=='),
     },
   },
   flags: {
--- backend/config/server.js
+++ backend/config/server.js
@@ -2,9 +2,13 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: [
+      '89jeK9pUuCsqg7te+zDBDw==',  
+      'Y+GemTlRs2QtV0RmY/Cprw=='   
+    ],
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
+
